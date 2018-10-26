826533d3ceb7f6a921149bec999a4923727f2bb5
denghanhao <610024180@qq.com>
2018-10-26 09:24:06 +0800

    .

--- app/src/main/java/com/danqiu/myapplication/activity/BaseActivity.java
+++ app/src/main/java/com/danqiu/myapplication/activity/BaseActivity.java
@@ -10,7 +10,7 @@ import com.danqiu.myapplication.views.ProgressDialogFragment;
 
 /**
  * Created by Administrator on 2018/9/19.
- * 基类配置常用1
+ * 基类配置常用11
  */
 
 abstract class BaseActivity extends AppCompatActivity {
