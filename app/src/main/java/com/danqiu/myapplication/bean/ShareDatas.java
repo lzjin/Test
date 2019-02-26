6a48a843504326514f33a9698863cb6105dff034
denghanhao <610024180@qq.com>
2019-02-26 11:15:57 +0800

    更新

---
+++ app/src/main/java/com/danqiu/myapplication/bean/ShareDatas.java
@@ -0,0 +1,68 @@
+package com.danqiu.myapplication.bean;
+
+public class ShareDatas {
+
+    private int shareType = 0;
+    private String title = "分享";
+    private String text = "分享内容...";
+    private String imagePath;
+    private String imageUrl="https://www.danqiuedu.com";
+    private String target_url="https://www.danqiuedu.com/action/v2/app";
+    private boolean inProgress = false;
+
+    public int getShareType() {
+        return shareType;
+    }
+
+    public void setShareType(int shareType) {
+        this.shareType = shareType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        // this.imageUrl = imageUrl;
+    }
+
+    public String getTarget_url() {
+        return target_url;
+    }
+
+    public void setTarget_url(String target_url) {
+        //  this.target_url = target_url;
+    }
+
+    public boolean isInProgress() {
+        return inProgress;
+    }
+
+    public void setInProgress(boolean inProgress) {
+        this.inProgress = inProgress;
+    }
+}
