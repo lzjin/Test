9c05de65b9121062fbed853d9c907ddb22600fcf
denghanhao <610024180@qq.com>
2020-03-27 14:58:00 +0800

    Glide磁盘清除

--- app/src/main/java/com/danqiu/myapplication/activity/FrescoActivity.java
+++ app/src/main/java/com/danqiu/myapplication/activity/FrescoActivity.java
@@ -1,9 +1,11 @@
 package com.danqiu.myapplication.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.danqiu.myapplication.R;
 import com.danqiu.myapplication.config.Constants;
@@ -31,6 +33,12 @@ public class FrescoActivity extends BaseActivity {
     ImageView ivRadiusTop;
     @BindView(R.id.iv_Circle)
     ImageView ivCircle;
+    @BindView(R.id.tv_cache_size)
+    TextView tvCacheSize;
+    @BindView(R.id.bt_cache)
+    Button btCache;
+    @BindView(R.id.bt_cache2)
+    Button btCache2;
     private String apkPath = Constants.SOFT_FILE_PATH + Constants.HEAD_DOWN_NAME;
     private String url = "http://www.people.com.cn/mediafile/pic/20161022/76/4315084153778263996.jpg";
 
@@ -66,28 +74,43 @@ public class FrescoActivity extends BaseActivity {
         ImageLoader.loadImage(simpleCircle, url);//圆图
         ImageLoader.loadImageBlur(simpleGaussianBlur, url);//高斯模糊
 
-        GlideUtils.loadImage(this,url,ivGlide);
-        GlideUtils.loadImageCorners(this,url,20,ivRadius);
-        GlideUtils.loadImageCorners(this,url,20,RoundedCornersTransformation.CornerType.TOP,ivRadiusTop);
-        GlideUtils.loadCircleImage(this,url,ivCircle);
+        tvCacheSize.setText(GlideUtils.getCacheSize(this));
+        GlideUtils.loadImage(this, url, ivGlide);
+        GlideUtils.loadImageCorners(this, url, 20, ivRadius);
+        GlideUtils.loadImageCorners(this, url, 20, RoundedCornersTransformation.CornerType.TOP, ivRadiusTop);
+        GlideUtils.loadCircleImage(this, url, ivCircle);
 
     }
 
 
-    /**
-     * 事件 下载
-     */
-    @OnClick(R.id.bt_download)
-    public void onViewClicked() {
-        loginModel = null;
-        loginModel = new LoginModel(this, dialog);
-        //loginModel.getEvaluationList("vRgLZ1h3us5qiwN+Vu/GReeiDoAEF40s", 1, 3);
-        loginModel.downloadFile("http://img-1253650823.cosgz.myqcloud.com/dqonline.apk", apkPath, Constants.APP_FILE_NAME_APK, progressBar);
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    @OnClick({R.id.bt_download, R.id.bt_cache, R.id.bt_cache2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bt_download:
+                loginModel = new LoginModel(this, dialog);
+                //loginModel.getEvaluationList("vRgLZ1h3us5qiwN+Vu/GReeiDoAEF40s", 1, 3);
+                loginModel.downloadFile("http://img-1253650823.cosgz.myqcloud.com/dqonline.apk", apkPath, Constants.APP_FILE_NAME_APK, progressBar);
+                break;
+            case R.id.bt_cache://内存清除
+                if(GlideUtils.clearMemoryCache(this)){
+                    tvCacheSize.setText(GlideUtils.getCacheSize(this));
+                }
+                break;
+            case R.id.bt_cache2://磁盘清除
+                GlideUtils.clearFileCache(this, new GlideUtils.CacheCallback() {
+                    @Override
+                    public void onCacheCallback() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvCacheSize.setText(GlideUtils.getCacheSize(FrescoActivity.this));
+                            }
+                        });
+                    }
+                });
 
+                break;
+        }
     }
 }
--- app/src/main/java/com/danqiu/myapplication/glide/GlideUtils.java
+++ app/src/main/java/com/danqiu/myapplication/glide/GlideUtils.java
@@ -2,6 +2,7 @@ package com.danqiu.myapplication.glide;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Looper;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -10,6 +11,9 @@ import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.danqiu.myapplication.R;
+
+import java.io.File;
+import java.math.BigDecimal;
 
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
@@ -32,7 +36,8 @@ public class GlideUtils {
                 .transition(DrawableTransitionOptions.withCrossFade(500)) // 渐变
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
-                .skipMemoryCache(true)//关闭缓存
+                .skipMemoryCache(true)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
     }
     /**
@@ -49,7 +54,8 @@ public class GlideUtils {
                 .transition(DrawableTransitionOptions.withCrossFade(500)) // 渐变
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)//缓存方式
+                .skipMemoryCache(false)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
     }
 
@@ -68,7 +74,8 @@ public class GlideUtils {
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
                 .transform(new CircleCrop())  // 圆角图片
-                .skipMemoryCache(true)//关闭缓存
+                .skipMemoryCache(true)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
     }
     /**
@@ -86,7 +93,8 @@ public class GlideUtils {
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
                 .transform(new CircleCrop())  // 圆角图片
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)//缓存方式
+                .skipMemoryCache(false)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
     }
 
@@ -105,7 +113,8 @@ public class GlideUtils {
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
                 .transform(new RoundedCorners(cornersSize)) //
-                .skipMemoryCache(true)//关闭缓存
+                .skipMemoryCache(true)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
 
     }
@@ -125,7 +134,8 @@ public class GlideUtils {
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
                 .transform(new RoundedCorners(cornersSize)) //
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)//缓存方式
+                .skipMemoryCache(false)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
     }
 
@@ -153,7 +163,8 @@ public class GlideUtils {
                 .transform(new RoundedCornersTransformation(cornersSize, 0,cornerType))  // 圆角图片
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
-                .skipMemoryCache(true)//关闭缓存
+                .skipMemoryCache(true)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
 
     }
@@ -182,7 +193,8 @@ public class GlideUtils {
                 .transform(new RoundedCornersTransformation(cornersSize, 0,cornerType))  // 圆角图片
                 .placeholder(R.mipmap.ic_launcher_round)// 加载中图片
                 .error(R.mipmap.ic_launcher_round) // 加载失败图片
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)//缓存方式
+                .skipMemoryCache(true)//关闭内存缓存
+                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )//设置磁盘缓存
                 .into(imageView);
     }
 
@@ -199,7 +211,6 @@ public class GlideUtils {
                 .centerCrop()
                 .dontAnimate()//防止设置placeholder导致第一次不显示网络图片,只显示默认图片的问题
                 .placeholder(defaultImg)
-                .skipMemoryCache(true)//关闭缓存
                 .into(imageview);
     }
     /**
@@ -215,7 +226,6 @@ public class GlideUtils {
                 .centerCrop()
                 .dontAnimate()//防止设置placeholder导致第一次不显示网络图片,只显示默认图片的问题
                 .placeholder(defaultImg)
-                .skipMemoryCache(true)//关闭缓存
                 .into(imageview);
     }
     /**
@@ -231,7 +241,6 @@ public class GlideUtils {
                 .centerCrop()
                 .dontAnimate()//防止设置placeholder导致第一次不显示网络图片,只显示默认图片的问题
                 .placeholder(defaultImg)
-                .skipMemoryCache(true)//关闭缓存
                 .into(imageview);
     }
 
@@ -245,27 +254,114 @@ public class GlideUtils {
                 .placeholder(defaultImg)
                 .dontAnimate()//防止设置placeholder导致第一次不显示网络图片,只显示默认图片的问题
                 .override(Width, Height)
-                .diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(imageview);
+                .into(imageview);
     }
 
     /**
-     * 清理内存缓存 在UI主线程中进行
+     * 清理内存  在UI主线程中进行
      * @param context
      */
-    public static void clearMemoryCache(Context context){
-        GlideApp.get(context).clearMemory();
+    public static boolean  clearMemoryCache(Context context){
+        try {
+            if (Looper.myLooper() == Looper.getMainLooper()) { //只能在主线程执行
+                GlideApp.get(context).clearMemory();
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     /**
      * 清理磁盘缓存 必须在子线程中执行
      * @param context
      */
-    public static void clearFileCache(Context context){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+    public static boolean clearFileCache(Context context, CacheCallback callback){
+        try {
+            if (Looper.myLooper() == Looper.getMainLooper()) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        GlideApp.get(context).clearDiskCache();
+                        if(callback!=null){
+                            callback.onCacheCallback();
+                        }
+                    }
+                }).start();
+            }else {
                 GlideApp.get(context).clearDiskCache();
+                if(callback!=null){
+                    callback.onCacheCallback();
+                }
             }
-        }).start();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
+
+    public static interface CacheCallback {
+        void onCacheCallback();
+    }
+    /**
+     * 获取Glide磁盘缓存大小
+     * @param mContext
+     * @return
+     */
+    public static String getCacheSize(Context mContext) {
+        try {
+            return getFormatSize(getFolderSize(new File(mContext.getCacheDir() + "/GlideCacheFolder" )));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "获取失败";
+        }
+    }
+
+    // 获取指定文件夹内所有文件大小的和
+    private static long getFolderSize(File file) throws Exception {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (File aFileList : fileList) {
+                if (aFileList.isDirectory()) {
+                    size = size + getFolderSize(aFileList);
+                } else {
+                    size = size + aFileList.length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    // 格式化单位
+    private static String getFormatSize(double size) {
+        double kiloByte = size / 1024;
+        if (kiloByte < 1) {
+            return size + "Byte";
+        }
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
+        }
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
+        }
+        double teraBytes = gigaByte / 1024;
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
+    }
+
+
+
 }
